3ea39ed7e9f5892583459d527b4605cb9473f298
tranqil <edoomik@gmail.com>
2011-10-07 22:35:39 +0200

    add args, opts, classes

--- utwhisper.py
+++ utwhisper.py
@@ -4,68 +4,20 @@ import re
 import json
 import sys
 
+# TODO
+# * addfile method. laziness.
+# * at TorrentFiles, TorrentJob, TorrentProperties some types need to
+#       converted into more friendly-human format. i.e size of file from bytes to MB
+# * refactoring.
+# * arguments and options implementation is ugly.
+# * add exception for stability.
 
-class TorrentProperties:
-        """
-        This class contains all properties for all torrents.
+# timeout error:
+# urllib2.URLError: <urlopen error [Errno 10060] A connection attempt failed becau
+# se the connected party did not properly respond after a period of time, or estab
+# lished connection failed because connected host has failed to respond>
+                
 
-        TODO: some properties contain values in bytes.
-                convert them to mb, kb/s and etc.
-                damn, it's so boring task.
-
-        data stucture: [
-                        {'hash': '123..', 'name': 'torrent 1'},
-                        {'hash': '351..', 'name': 'torrent 2'
-                        ]
-                        
-        """
-        
-        def __init__(self, torrents_json):
-                self.torrents = []
-                for each in torrents_json:
-                        self.torrents.append(
-                                {
-                                'hash': each[0],
-                                'status': each[1],
-                                'name': each[2],
-                                'size': each[3],
-                                'percent progress': each[4],
-                                'downloaded': each[5],
-                                'uploaded': each[6],
-                                'ratio': each[7],
-                                'upload speed': each[8],
-                                'download speed': each[9],
-                                'eta': each[10],
-                                'label': each[11],
-                                'peers connected': each[12],
-                                'peers in swarm': each[13],
-                                'seeds connected': each[14],
-                                'availability': each[15],
-                                'torrent queue order': each[16],
-                                'remaining': each[17],
-                                'download url': each[18],
-                                'rss feed url': each[19],
-                                'status message': each[20],
-                                'stream id': each[21],
-                                'added on': each[22],
-                                'completed on': each[23],
-                                'app update url': each[24]
-                                 }
-                                )
-
-        def print_all(self):
-        """
-        prints all torrents.
-        adjust this method output for your needs.
-        """
-                for each in self.torrents:
-                        print """
-name: %s
-size: %s
-percent: %s
-""" % (each['name'], each['size'], each['percent progress'])
-        
-                        
 class Torrent:
     
     def __init__(self):
@@ -150,11 +102,19 @@ class Torrent:
 
     def getfiles(self, torrent_index):
         """ API: action=getfiles&hash=[TORRENT HASH] """
-        print self.__request("&action=getfiles&hash=" + self.index2hash(torrent_index))
+
+        files_json = json.loads(self.__request("&action=getfiles&hash=" + self.index2hash(torrent_index)))
+        files = TorrentFiles(files_json)
+        files.print_files()
+        #return self.__request("&action=getfiles&hash=" + self.index2hash(torrent_index))
 
     def torrentprops(self, torrent_index):
         """ API:  action=getprops&hash=[TORRENT HASH] """
-        print self.__request("&action=getprops&hash=" + self.index2hash(torrent_index))
+        #print self.__request("&action=getprops&hash=" + self.index2hash(torrent_index))
+        #props_json = json.loads(self.__request("&action=getprops&hash=" + self.index2hash(torrent_index)))['props']
+        props_dict = json.loads(self.__request("&action=getprops&hash=" + self.index2hash(torrent_index)))['props'][0]
+        props = TorrentJob(props_dict)
+        props.print_props()
 
     def start(self, torrent_index):
         """ API: action=start&hash=[TORRENT HASH] """
@@ -196,9 +156,17 @@ class Torrent:
         """ API: action=removedatatorrent&hash=[TORRENT HASH] """
         print self.__request("&action=removedatatorrent&hash=" + self.index2hash(torrent_index))
 
-    def setprio(self, torrent_index):
-        """ API: action=setprio&hash=[TORRENTHASH]&p=[PRIORITY]&f=[FILE INDEX] """
-        print self.__request("action=setprio&hash=[TORRENTHASH]&p=[PRIORITY]&f=[FILE INDEX]")
+    def setprio(self, torrent_index, priority, file_index):
+        """
+        API: action=setprio&hash=[TORRENTHASH]&p=[PRIORITY]&f=[FILE INDEX]
+        0 = Don't Download
+        1 = Low Priority
+        2 = Normal Priority
+        3 = High Priority
+        """
+        #print self.__request("action=setprio&hash=[TORRENTHASH]&p=[PRIORITY]&f=[FILE INDEX]")
+        print self.__request("&action=setprio&hash=" + self.index2hash(torrent_index)
+                       + "&p=" + str(priority) + "&f=" + str(file_index))
 
     def getxferhist(self):
         """ API: action=getxferhist """
@@ -212,7 +180,7 @@ class Torrent:
         """ API: action=getversion """
         print self.__request("&action=getversion")
 
-    def addurl(self, url):
+    def addurl(self, torrent_url):
         """ API: action=add-url&s=[TORRENT URL] """
         # optional
         # &download_dir=<integer>
@@ -221,6 +189,9 @@ class Torrent:
         print self.__request("&action=add-url&s=" + torrent_url)
 
     def addfile(self):
+        # TODO
+        # lazy to implement this.
+        # http://www.doughellmann.com/PyMOTW/urllib2/
         """ API: action=add-file """
         # action=add-file
         pass
@@ -230,3 +201,206 @@ class Torrent:
         #action=list-dirs
         pass
 
+
+
+    def request(self, action):
+        """
+        pass your own request
+        """
+        pass
+
+
+
+class TorrentProperties:
+        """
+        This class contains all properties for all torrents.
+
+        TODO: some properties contain values in bytes.
+                convert them to mb, kb/s and etc.
+                damn, it's so boring task.
+
+        data stucture: [
+                        {'hash': '123..', 'name': 'torrent 1'},
+                        {'hash': '351..', 'name': 'torrent 2'
+                        ]
+                        
+        """
+        
+        def __init__(self, torrents_json):
+                self.torrents = []
+                for each in torrents_json:
+                        self.torrents.append(
+                                {
+                                'hash': each[0],
+                                'status': each[1],
+                                'name': each[2],
+                                'size': each[3],
+                                'percent progress': each[4],
+                                'downloaded': each[5],
+                                'uploaded': each[6],
+                                'ratio': each[7],
+                                'upload speed': each[8],
+                                'download speed': each[9],
+                                'eta': each[10],
+                                'label': each[11],
+                                'peers connected': each[12],
+                                'peers in swarm': each[13],
+                                'seeds connected': each[14],
+                                'availability': each[15],
+                                'torrent queue order': each[16],
+                                'remaining': each[17],
+                                'download url': each[18],
+                                'rss feed url': each[19],
+                                'status message': each[20],
+                                'stream id': each[21],
+                                'added on': each[22],
+                                'completed on': each[23],
+                                'app update url': each[24]
+                                 }
+                                )
+        def print_all(self):
+                """
+                prints all torrents.
+                adjust this method output for your needs.
+                """
+                index = 0
+                for each in self.torrents:
+                        print """
+index: %s
+name: %s
+size: %s
+completed: %s
+""" % (index, each['name'], each['size'], each['percent progress'])
+                        index += 1
+                        
+
+class TorrentFiles:
+        """
+        Class contains properties for torrent files.
+        """
+        def __init__(self, files_json):
+                  self.files  = []
+                  self.thash = files_json['files'][0]
+                  for each in files_json['files'][1]:
+                          self.files.append(
+                                  {
+                                          'filename': each[0],
+                                          'filesize': each[1],
+                                          'downloaded': each[2],
+                                          'priority': each[3],
+                                          'first piece': each[4],
+                                          'num pieces': each[5],
+                                          'streamable': each[6],
+                                          'encoded rate': each[7],
+                                          'duration': each[8],
+                                          'width': each[9],
+                                          'height': each[10],
+                                          'stream eta': each[11],
+                                          'streamability': each[12]
+                                          }
+                                  )
+        def print_files(self):
+                """ print all files of this torrent """
+
+                print "hash: " + self.thash
+                for each in self.files:
+                        print """
+file: %s
+size: %s
+downloaded: %s
+priority: %s
+""" % (each['filename'], each['filesize'], each['downloaded'], each['priority'])
+
+
+
+class TorrentJob:
+        """
+        Class contains properties for torrent job.
+        """
+
+        def __init__(self, props_dict):
+                self.props = {}
+                self.props = props_dict
+
+        def print_props(self):
+                """ print torrent job properties """
+                print self.props['hash']
+                print self.props['trackers']
+                
+                # and etc.
+
+
+# handle arguments and options.
+
+args = sys.argv[1:]
+torrent = Torrent()
+
+if '--hashtable' in args:
+        torrent.hashtable()
+        
+elif '--indextohash' in args:
+        torrent.index2hash(args[2])
+        
+elif '--torrents-list' in args:
+        torrent.torrents_list()
+        
+elif '--get-settings' in args:
+        torrent.getsettings()
+        
+elif '--get-files' in args:
+        torrent.getfiles(args[2])
+        
+elif '--torrent-props' in args:
+        torrent.torrentprops(args[2])
+
+elif '--start' in args:
+        torrent.start(args[2])
+        
+elif '--stop' in args:
+        torrent.stop(args[2])
+        
+elif '--pause' in args:
+        torrent.pause(args[2])
+        
+elif '--force-start' in args:
+        torrent.forcestart(args[2])
+        
+elif '--unpause' in args:
+        torrent.unpause(args[2])
+        
+elif '--recheck' in args:
+        torrent.recheck(args[2])
+        
+elif '--remove' in args:
+        torrent.remove(args[2])
+        
+elif '--remove-data' in args:
+        torrent.removedata(args[2])
+        
+elif '--remove-torrent' in args:
+        torrent.removetorrent(args[2])
+        
+elif '--remove-data-torrent' in args:
+        torrent.removedatatorrent(args[2])
+
+elif '--set-prio' in args:
+        torrent.setprio(args[2], args[3], args[4]) # torrent_index, priority, file_index
+        
+elif '--get-hist' in args:
+        torrent.getxferhist()
+        
+elif '--reset-hist' in args:
+        torrent.resetxferhist()
+        
+elif '--get-version' in args:
+        torrent.getversion()
+        
+elif '--add-url' in args:
+        torrent.addurl(args[2])
+        
+elif '--list-dirs' in args:
+        torrent.listdirs()
+        
+elif '--req' in args:
+        torrent.request(args[2])
+        
